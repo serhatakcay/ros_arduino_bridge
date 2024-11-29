f2528b79965bfb6a88729fe6b63b1b00c6884151
Serhat <71022496+serhatakcay@users.noreply.github.com>
2024-11-29 19:16:38 +0000

    Update motor_driver.h

--- ROSArduinoBridge/motor_driver.h
+++ ROSArduinoBridge/motor_driver.h
@@ -3,12 +3,12 @@
    *************************************************************/
 
 #ifdef L298_MOTOR_DRIVER
-  #define RIGHT_MOTOR_BACKWARD 5
-  #define LEFT_MOTOR_BACKWARD  6
+  #define RIGHT_MOTOR_BACKWARD 10
   #define RIGHT_MOTOR_FORWARD  9
-  #define LEFT_MOTOR_FORWARD   10
-  #define RIGHT_MOTOR_ENABLE 12
-  #define LEFT_MOTOR_ENABLE 13
+  #define LEFT_MOTOR_BACKWARD  6
+  #define LEFT_MOTOR_FORWARD   5
+  #define RIGHT_MOTOR_ENABLE 11
+  #define LEFT_MOTOR_ENABLE 3
 #endif
 
 void initMotorController();
