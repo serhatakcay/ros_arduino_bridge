03f0a2caa53a8cbf6b8857edd06422a5d3be2959
Serhat <71022496+serhatakcay@users.noreply.github.com>
2024-11-29 20:24:21 +0000

    Update encoder_driver.h

--- ROSArduinoBridge/encoder_driver.h
+++ ROSArduinoBridge/encoder_driver.h
@@ -6,8 +6,8 @@
 #ifdef ARDUINO_ENC_COUNTER
   //below can be changed, but should be PORTD pins; 
   //otherwise additional changes in the code are required
-  #define LEFT_ENC_PIN_A PD4  //pin 4
-  #define RIGHT_ENC_PIN_B PD7  //pin 7  
+  #define LEFT_ENC_PIN PD4  //pin 4
+  #define RIGHT_ENC_PIN PD7  //pin 7  
 //#define LEFT_ENC_PIN_B PD3  //pin 3
   
   //below can be changed, but should be PORTC pins
